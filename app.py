73ef7e3ade0727558cf8da22718e78bd7ba911d7
HananKibria <60578784+HananKibria@users.noreply.github.com>
2021-01-01 18:22:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import re
 from flask import Flask, request
 import telegram
 from telebot.credentials import bot_token, bot_user_name,URL
-from telegram import message,user,InlineQuery
+
+
 global bot
 global TOKEN
 TOKEN = bot_token
 bot = telegram.Bot(token=TOKEN)
 
-# start the flask app
 app = Flask(__name__)
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
@@ -18,8 +18,7 @@ def respond():
 
    chat_id = update.message.chat.id
    msg_id = update.message.message_id
-   location=InlineQuery.location
-   print(location)
+
    # Telegram understands UTF-8, so encode text for unicode compatibility
    text = update.message.text.encode('utf-8').decode()
    # for debugging purposes only
@@ -28,7 +27,7 @@ def respond():
    if text == "/start":
        # print the welcoming message
        bot_welcome = """
-       Welcome to cool bot
+       Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
        """
        # send the welcoming message
        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
@@ -49,21 +48,18 @@ def respond():
 
    return 'ok'
 
-@app.route('/setwebhook', methods=['GET', 'POST'])
+@app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
-    # we use the bot object to link the bot to our app which live
-    # in the link provided by URL
-    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
-    # something to let us know things work
-    if s:
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
+   s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+   if s:
+       return "webhook setup ok"
+   else:
+       return "webhook setup failed"
 
 @app.route('/')
 def index():
-    return '.'
+   return '.'
+
+
 if __name__ == '__main__':
-    # note the threaded arg which allow
-    # your app to have more than one thread
-    app.run(threaded=True)
+   app.run(threaded=True)
